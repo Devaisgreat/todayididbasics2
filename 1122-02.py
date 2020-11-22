6f9a3415cd4e778974d0ffce68ac8a4a4743367f
DEVA <74277700+Devaisgreat@users.noreply.github.com>
2020-11-22 18:14:36 +0900

    ididbasics

---
+++ 1122-02.py
@@ -0,0 +1,22 @@
+# Data Types
+
+# String
+
+print("Hello"[4])
+
+# subscript
+
+print("hello" + "world")
+
+# integer
+
+print(123 + 345)
+
+# float
+
+3.14159
+
+# floating point number
+
+# boolean
+# True/ False
---
+++ 1122-05.py
@@ -0,0 +1,18 @@
+# mathematical operations in python
+
+3 + 5
+7 - 3
+3 * 2
+6 / 3
+2 ** 3
+# ** : exponent
+
+# # PEMDAS
+# Parenthesis : ()
+# Exponents : **
+# Multiplication : *
+# Division : /
+# Addition : +
+# Subtraction : -
+
+print(3 * 3 + 3 / 3 - 3)
